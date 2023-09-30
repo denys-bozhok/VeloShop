78c7dadaf8ef089412e4f76354ed0d0fc0759e38
denys-bozhok <denys.already@gmail.com>
2023-09-30 07:58:29 +0300

    models fix

--- products/admin.py
+++ products/admin.py
@@ -1,29 +1,36 @@
 from django.contrib import admin
 from .models import *
 
- 
- 
 
-admin.site.register(Color)
-admin.site.register(FrameSize)
-admin.site.register(WheelSize)
-admin.site.register(Characteristic)
 admin.site.register(Manufacturer)
+admin.site.register(Color)
+admin.site.register(Size)
+admin.site.register(WheelSize)
+admin.site.register(SuspensionTravel)
+admin.site.register(Material)
+admin.site.register(Weight)
+admin.site.register(Characteristic)
 
 
-class BycicleGaleryAdmin(admin.StackedInline):
+class BicycleGaleryAdmin(admin.StackedInline):
     model = BicycleGalery
- 
 @admin.register(Bicycle)
-class BicycleAdmin(admin.ModelAdmin):
-    inlines = [BycicleGaleryAdmin]
- 
+class BycicleAdmin(admin.ModelAdmin):
+    inlines = [BicycleGaleryAdmin]
     class Meta:
        model = Bicycle
- 
 @admin.register(BicycleGalery)
 class BycicleGaleryAdmin(admin.ModelAdmin):
     pass
 
 
-
+class HamletGaleryAdmin(admin.StackedInline):
+    model = HamletGalery
+@admin.register(Hamlet)
+class HamletAdmin(admin.ModelAdmin):
+    inlines = [HamletGaleryAdmin]
+    class Meta:
+       model = Hamlet
+@admin.register(HamletGalery)
+class HamletGaleryAdmin(admin.ModelAdmin):
+    pass
--- products/models.py
+++ products/models.py
@@ -9,14 +9,11 @@ import os
 #* -----WRAPPERS-----
 #? it`s for rename image of product and create folder by articul 
 def products_filename_wrapper(instance, filename):
-    
     ext = filename.split('.')[-1]
     filename = '{}.{}'.format(instance.article, ext)
-
 #? for delete image, if filename is reserved
     # if Product.objects.get(image=f"images/products/{filename}"):
     #     Product.objects.get(image=f"images/products/{filename}").image.delete(save=True)
-    
     return os.path.join(f'images/products/{instance.article}/', filename)
 
 
@@ -26,10 +23,7 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=20, unique=True)
     image = models.FileField(upload_to='images/manufactirers/')
     description = models.TextField(blank=True, null=True)
-    
-    slug = models.CharField(max_length=30, 
-                            editable=False, 
-                            auto_created=True)
+    slug = models.CharField(max_length=30, editable=False, auto_created=True)
 
     def save(self):
         self.slug = slugify(self.name)
@@ -42,29 +36,45 @@ class Manufacturer(models.Model):
 class Color(models.Model):
     first_color = models.CharField(max_length=10)
     second_color = models.CharField(max_length=10, blank=True, null=True)
-    
+
     def __str__(self):
         return f'{self.first_color}, {self.second_color}'
 
 
-class FrameSize(models.Model):
-    size = models.DecimalField(max_digits=7, 
-                               decimal_places=1,
-                               unique=True,
-                               validators=[MinValueValidator(6),],)
-
+class Size(models.Model):
+    short_name = models.CharField(max_length=5)
+    minimal = models.IntegerField(validators=[MinValueValidator(0)])
+    maximal = models.IntegerField(validators=[MinValueValidator(0)])
+    
     def __str__(self):
-        return f'{self.size}'
+        return f'{self.short_name} ({self.minimal} - {self.minimal}cm)'
 
 
 class WheelSize(models.Model):
-    size = models.DecimalField(max_digits=7, 
-                               decimal_places=1,
-                               unique=True,
-                               validators=[MinValueValidator(6),],)
+    size = models.DecimalField(max_digits=7, decimal_places=1, unique=True, validators=[MinValueValidator(6),])
 
     def __str__(self):
         return f'{self.size}'
+
+class SuspensionTravel(models.Model):
+    suspension_travel = models.FloatField(validators=[MinValueValidator(0),],)
+
+    def __str__(self):
+        return f'{self.suspension_travel}mm'
+
+
+class Material(models.Model):
+    name = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return f'{self.name}'
+    
+
+class Weight(models.Model):
+    weight = models.FloatField(validators=[MinValueValidator(0),])
+    
+    def __str__(self):
+        return f'{self.weight}g'
 
 
 class Characteristic(models.Model):
@@ -75,42 +85,19 @@ class Characteristic(models.Model):
         return f'{self.name}: {self.description}'
 
 
-
 # * -----PRODUCTS-----
 class Product(models.Model):
-    manufacturer = models.ForeignKey(Manufacturer,
-                                     blank=True,
-                                     null=True, 
-                                     on_delete=models.DO_NOTHING)
-    
+    manufacturer = models.ForeignKey(Manufacturer, blank=True,null=True, on_delete=models.DO_NOTHING)
     label = models.CharField(max_length=30)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True, null=True)
     article = models.CharField(max_length=30, unique=True)
-
     characteristics = models.ManyToManyField(Characteristic, blank=True)
-    
-    category = models.ForeignKey(Category, 
-                                 on_delete=models.DO_NOTHING, 
-                                 blank=True, 
-                                 null=True)
-    
-    subcategory = models.ForeignKey(SubCategory, 
-                                 on_delete=models.DO_NOTHING, 
-                                 blank=True, 
-                                 null=True)
-    
-    rating = models.FloatField(default=0,
-                               validators=[MinValueValidator(0), 
-                                           MaxValueValidator(5)])
-    
-    value = models.IntegerField(default=0, 
-                                validators=[MinValueValidator(0)])
-    
-    slug = models.CharField(max_length=30, 
-                            editable=False, 
-                            auto_created=True)
-    
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, blank=True, null=True)
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.DO_NOTHING, blank=True, null=True)
+    rating = models.FloatField(default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    value = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    slug = models.CharField(max_length=30, editable=False, auto_created=True)
     image = models.FileField(upload_to=products_filename_wrapper, blank=True)
     
     def save(self):
@@ -127,20 +114,17 @@ class Product(models.Model):
 class Bicycle(Product):
     color = models.ForeignKey(Color, on_delete=models.DO_NOTHING)
     wheel = models.ForeignKey(WheelSize, on_delete=models.DO_NOTHING)
-    frame = models.ForeignKey(FrameSize, on_delete=models.DO_NOTHING)
-    
-    year = models.IntegerField(validators=[MinValueValidator(2000)], 
-                               blank=True, 
-                               null=True)
+    frame_size = models.ForeignKey(Size, on_delete=models.DO_NOTHING)
+    frame_material = models.ForeignKey(Material, on_delete=models.DO_NOTHING)
+    suspension = models.BooleanField(default=False)
+    suspension_travel = models.ForeignKey(SuspensionTravel, on_delete=models.DO_NOTHING)
+    weight = models.ForeignKey(Weight, on_delete=models.DO_NOTHING, blank=True, null=True)
+    year = models.IntegerField(validators=[MinValueValidator(2000)], blank=True, null=True)
     
 
 class BicycleGalery(models.Model):
     product = models.ForeignKey(Bicycle, default=None, on_delete=models.CASCADE)
-
-    article = models.CharField(max_length=30, 
-                            editable=False, 
-                            auto_created=True)
-    
+    article = models.CharField(max_length=30, editable=False, auto_created=True)
     image = models.FileField(upload_to=products_filename_wrapper, blank=True)
  
     def __str__(self):
@@ -150,8 +134,24 @@ class BicycleGalery(models.Model):
         self.article = self.product.article
         super().save()
     
-class Helmet(Product):
-    pass
+
+class Hamlet(Product):
+    color = models.ForeignKey(Color, on_delete=models.DO_NOTHING)
+    size = models.ForeignKey(Size, on_delete=models.DO_NOTHING)
+    weight = models.ForeignKey(Weight, on_delete=models.DO_NOTHING, blank=True, null=True)
+
+
+class HamletGalery(models.Model):
+    product = models.ForeignKey(Hamlet, default=None, on_delete=models.CASCADE)
+    article = models.CharField(max_length=30,editable=False, auto_created=True)
+    image = models.FileField(upload_to=products_filename_wrapper, blank=True)
+
+    def __str__(self):
+        return self.product.article
+    
+    def save(self):
+        self.article = self.product.article
+        super().save()
 
 
 class Lighting(Product):
