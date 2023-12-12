7203c876fb42a569c4fd798c74ccbf2f7e9f3a05
denys-bozhok <denys.already@gmail.com>
2023-12-12 18:16:09 +0200

    add paginator

--- app/utilites.py
+++ app/utilites.py
@@ -1,12 +1,21 @@
-from products.models import ProductsQuerySet
+from django.core.paginator import Paginator
 
+from products.models import ProductsQuerySet
 from carts.utilites import for_cart_detail
 from .models import SiteNavigation, SocialNetwork, FavoritesAndOther, Language, SubCategory, CharterQuerySet, Category, Chapter
 from carts.utilites import for_cart_detail
 
-#* -----GET DICTIONARY FOR TEMPLATE-----
 
-def subheader(req) -> dict:
+# * -----GET DICTIONARY FOR TEMPLATE-----
+def paginator(page: int, products: dict, per_page: int) -> object:
+
+    paginator = Paginator(products, per_page)
+    products_paginator = paginator.page(page)
+
+    return products_paginator
+
+
+def subheader(req: object) -> dict:
     return {'abouts': CharterQuerySet.all_models(SiteNavigation),
             'social_networks': CharterQuerySet.all_models(SocialNetwork),
             'favorites_and_other': CharterQuerySet.all_models(FavoritesAndOther),
@@ -14,39 +23,53 @@ def subheader(req) -> dict:
             'cart_lenth': for_cart_detail(req)['items_quantity']}
 
 
-def for_categories(slug:str, model:object, *category_slug:str) -> dict:
+def for_categories(slug: str, model: object, *category_slug: str, page_number: int = 1) -> dict:
     match(model.__name__):
         case 'Chapter':
             chapter = model.objects.get(slug=slug)
             categories = Category.objects.filter(chapter=chapter)
-            products = list(filter(lambda product: product.category.chapter.slug == slug, ProductsQuerySet.all_products('')))
 
-            return {'categories': categories, 
-                    'chapter': chapter, 
-                    'title': chapter.name, 
-                    'products': products}
-        
+            products = list(filter(lambda product: product.category.chapter.slug ==
+                            slug, ProductsQuerySet.all_products('')))
+
+            products_paginator = paginator(page_number, products, 2)
+
+            return {'categories': categories,
+                    'chapter': chapter,
+                    'title': chapter.name,
+                    'products': products_paginator}
+
         case 'Category':
             category = model.objects.get(slug=slug)
             sub_categories = SubCategory.objects.filter(category=category)
-            products = list(filter(lambda product: product.category.slug == slug, ProductsQuerySet.all_products('')))
 
-            return {'sub_categories': sub_categories, 
-                    'category': category, 
-                    'title': category.name, 
-                    'products': products}
-        
-        case 'SubCategory':
-            sub_category = model.objects.get(slug=slug)
-            sub_categories = SubCategory.objects.filter(sub_category=sub_category)
-            category_slug = category_slug[0]
-            category = Category.objects.get(slug=category_slug)
-            products = list(filter(lambda product: product.subcategory.slug == slug, ProductsQuerySet.all_products('')))
+            products = list(filter(lambda product: product.category.slug ==
+                            slug, ProductsQuerySet.all_products('')))
+
+            products_paginator = paginator(page_number, products, 2)
 
             return {'sub_categories': sub_categories,
-                    'sub_category': sub_category, 
-                    'title': sub_category.name, 
-                    'products': products, 
+                    'category': category,
+                    'title': category.name,
+                    'products': products_paginator}
+
+        case 'SubCategory':
+            sub_category = model.objects.get(slug=slug)
+            sub_categories = SubCategory.objects.filter(
+                sub_category=sub_category)
+            category_slug = category_slug[0]
+            category = Category.objects.get(slug=category_slug)
+
+            products = list(filter(lambda product: product.subcategory.slug ==
+                            slug, ProductsQuerySet.all_products('')))
+
+            products_paginator = paginator(page_number, products, 2)
+
+            return {'sub_categories': sub_categories,
+                    'sub_category': sub_category,
+                    'title': sub_category.name,
+                    'products': products_paginator,
                     'category': category}
+
         case _:
             return
--- app/views.py
+++ app/views.py
@@ -4,36 +4,37 @@ from .models import SiteNavigation, Chapter, Category, SubCategory
 from .utilites import subheader, for_categories
 
 
-def home(req:object) -> classmethod:
+def home(req: object) -> classmethod:
     context = {'title': 'VeloShop', 'chapters': Chapter.objects.all()}
     context.update(subheader(req))
 
     return render(req, 'app/app.html', context)
 
 
-def abouts(req:object, about_slug:str) -> classmethod:
+def abouts(req: object, about_slug: str) -> classmethod:
     about = SiteNavigation.objects.get(slug=about_slug)
-    context = {'title': about.name, 'about':about}
+    context = {'title': about.name, 'about': about}
     context.update(subheader(req))
 
     return render(req, 'app/app.html', context)
 
 
-def chapters(req:object, chapter_slug:str) -> classmethod:
-    context = for_categories(chapter_slug, Chapter)
+def chapters(req: object, chapter_slug: str, *page_number: int) -> classmethod:
+    context = for_categories(chapter_slug, Chapter, page_number)
     context.update(subheader(req))
 
     return render(req, 'app/app.html', context)
 
 
-def categories(req:object, category_slug:str, ) -> classmethod:
-    context = for_categories(category_slug, Category)
+def categories(req: object, category_slug: str, page: int = 1) -> classmethod:
+    context = for_categories(category_slug, Category, page)
     context.update(subheader(req))
 
     return render(req, 'app/app.html', context)
 
 
-def sub_categories(req:object, category_slug:str, sub_category_slug:str) -> classmethod:
-    context = for_categories(sub_category_slug, SubCategory, category_slug)
+def sub_categories(req: object, category_slug: str, sub_category_slug: str, page: int = 1) -> classmethod:
+    context = for_categories(
+        sub_category_slug, SubCategory, category_slug, page)
     context.update(subheader(req))
     return render(req, 'app/app.html', context)
