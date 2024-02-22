05e46ddb4be641d7e70f4eac1d778b183a450fdf
denys-bozhok <55252438+denys-bozhok@users.noreply.github.com>
2024-02-22 19:31:47 +0200

    add postgres

--- Store/settings.py
+++ Store/settings.py
@@ -96,6 +96,14 @@ WSGI_APPLICATION = 'Store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
     'default': {
 
